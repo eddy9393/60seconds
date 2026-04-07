4c3dac90ef15a45f5f0107697c784299514a0ff1
eddy9393 <kroonstadt.earvin@gmail.com>
2026-04-07 16:04:17 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,11 @@
-
+window.APP_CONFIG = {
+  appName: '60 Seconds FM',
+  apiBaseUrl: '', // vul later in als je API hebt
+  storageBaseUrl: '', // vul later in voor audio/images
+  features: {
+    enableLikes: true,
+    enableSkips: true,
+    enableNotifications: true,
+    requireApproval: true
+  }
+};
